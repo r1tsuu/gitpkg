39fcf96572d62bc3ad8cfed723225131a74fc5cc
r1tsuu <enjoythepain1337@gmail.com>
2024-06-29 21:01:15 +0300

    pkg name run only once

--- src/tasks/Publish/get-git-tag-name.js
+++ src/tasks/Publish/get-git-tag-name.js
@@ -5,10 +5,26 @@ export default function getGitTagName(pkg, config) {
   return gitpkgPackageName;
 }
 
+function formatDate(date) {
+  const padZero = num => num.toString().padStart(2, '0');
+
+  const year = date.getFullYear();
+  const month = padZero(date.getMonth() + 1); // Months are zero-based in JavaScript
+  const day = padZero(date.getDate());
+  const hours = padZero(date.getHours());
+  const minutes = padZero(date.getMinutes());
+  const seconds = padZero(date.getSeconds());
+
+  return `${year}${month}${day}_${hours}${minutes}${seconds}`;
+}
+
+const now = new Date();
+const formattedDate = formatDate(now);
+
 /**
  * Returns the default tag name. This function can be replaced in the config file.
  * @param {object} pkg The package.json object.
  */
 export function defaultTagNameFormat(pkg) {
-  return `${normalisePackageNameNpm(pkg.name)}-v${pkg.version}-${Date.now()}`;
+  return `${normalisePackageNameNpm(pkg.name)}-v${pkg.version}-${formattedDate}`;
 }
